427325257d3238fce353fc3af59b879601addeae
Julio <38758281+Julioevm@users.noreply.github.com>
2024-01-23 15:49:14 +0000

    Fix cart missing div.

--- front-end/src/pages/ShoppingCartPage.vue
+++ front-end/src/pages/ShoppingCartPage.vue
@@ -3,8 +3,10 @@
   <div v-if="cartItems.length > 0">
     <div v-for="product in cartItems" class="product-container" :key="product.id">
       <img :src="product.imageName" class="product-image" />
-      <h3>{{ product.name }}</h3>
-      <p>{{ product.price }}</p>
+      <div class="details-wrap">
+        <h3>{{ product.name }}</h3>
+        <p>{{ product.price }}</p>
+      </div>
       <button class="remove-button">Remove from Cart</button>
     </div>
     <button class="checkout-button">Proceed to checkout</button>
